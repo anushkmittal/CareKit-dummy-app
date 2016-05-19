2c3dea4753a04d546b1da1d200bf03df1471327d
Anushk Mittal <ANUSHKMITTAL@ICLOUD.COM>
2016-05-18 23:38:55 -0700

    Initial Commit

---
+++ sampleCare/CarePlanStoreManager.swift
@@ -0,0 +1,87 @@
+//
+//  CarePlanStoreManager.swift
+//  sampleCare
+//
+//  Created by Anushk Mittal on 5/18/16.
+//  Copyright © 2016 Anushk Mittal. All rights reserved.
+//
+
+import CareKit
+
+class CarePlanStoreManager: NSObject {
+    // MARK: Static Properties
+    
+    static var sharedCarePlanStoreManager = CarePlanStoreManager()
+    
+    // MARK: Properties
+    
+    weak var delegate: CarePlanStoreManagerDelegate?
+    
+    let store: OCKCarePlanStore
+    
+   // var insights: [OCKInsightItem] {
+        //eturn insightsBuilder.insights
+   // }
+    
+  //  private let insightsBuilder: InsightsBuilder
+    
+    // MARK: Initialization
+    
+    private override init() {
+        // Determine the file URL for the store.
+        let searchPaths = NSSearchPathForDirectoriesInDomains(.ApplicationSupportDirectory, .UserDomainMask, true)
+        let applicationSupportPath = searchPaths[0]
+        let persistenceDirectoryURL = NSURL(fileURLWithPath: applicationSupportPath)
+        
+        if !NSFileManager.defaultManager().fileExistsAtPath(persistenceDirectoryURL.absoluteString, isDirectory: nil) {
+            try! NSFileManager.defaultManager().createDirectoryAtURL(persistenceDirectoryURL, withIntermediateDirectories: true, attributes: nil)
+        }
+        
+        // Create the store.
+        store = OCKCarePlanStore(persistenceDirectoryURL: persistenceDirectoryURL)
+        
+        /*
+         Create an `InsightsBuilder` to build insights based on the data in
+         the store.
+         */
+        //insightsBuilder = InsightsBuilder(carePlanStore: store)
+        
+        super.init()
+        
+        // Register this object as the store's delegate to be notified of changes.
+        store.delegate = self
+        
+        // Start to build the initial array of insights.
+        //updateInsights()
+    }
+    
+    /*
+    func updateInsights() {
+        insightsBuilder.updateInsights { [weak self] completed, newInsights in
+            // If new insights have been created, notifiy the delegate.
+            guard let storeManager = self, newInsights = newInsights where completed else { return }
+            storeManager.delegate?.carePlanStoreManager(storeManager, didUpdateInsights: newInsights)
+        }
+    }
+ */
+}
+
+
+
+extension CarePlanStoreManager: OCKCarePlanStoreDelegate {
+    func carePlanStoreActivityListDidChange(store: OCKCarePlanStore) {
+        //updateInsights()
+    }
+    
+    func carePlanStore(store: OCKCarePlanStore, didReceiveUpdateOfEvent event: OCKCarePlanEvent) {
+        //updateInsights()
+    }
+}
+
+
+
+protocol CarePlanStoreManagerDelegate: class {
+    
+    func carePlanStoreManager(manager: CarePlanStoreManager, didUpdateInsights insights: [OCKInsightItem])
+    
+}
---
+++ sampleCare/ViewController.swift
@@ -0,0 +1,94 @@
+//
+//  ViewController.swift
+//  sampleCare
+//
+//  Created by Anushk Mittal on 5/18/16.
+//  Copyright © 2016 Anushk Mittal. All rights reserved.
+//
+
+import UIKit
+import CareKit
+
+
+class ViewController: UIViewController {
+    
+    private let sampleData: SampleData
+
+    private let storeManager = CarePlanStoreManager.sharedCarePlanStoreManager
+
+   // private var careCardViewController: OCKCareCardViewController!
+
+    
+    
+    required init?(coder aDecoder: NSCoder)
+    
+    {
+        
+        sampleData = SampleData(carePlanStore: storeManager.store)
+
+        super.init(coder: aDecoder)
+
+     //   careCardViewController = createCareCardViewController()
+        
+        let careCardViewController = OCKCareCardViewController(carePlanStore: storeManager.store)
+        //self.navigationController?.pushViewController(careCardViewController, animated: true)
+        
+        storeManager.delegate = self
+
+    }
+    
+    /*
+    private func createCareCardViewController() -> OCKCareCardViewController {
+        let viewController = OCKCareCardViewController(carePlanStore: storeManager.store)
+        
+        // Setup the controller's title and tab bar item
+        viewController.title = NSLocalizedString("Care Card", comment: "")
+        //viewController.tabBarItem = UITabBarItem(title: viewController.title, image: UIImage(named:"carecard"), selectedImage: UIImage(named: "carecard-filled"))
+        return viewController
+    }
+    
+    */
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+      //  self.navigationController?.pushViewController(careCardViewController, animated: true)
+
+        
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    
+    @IBAction func click(sender: AnyObject) {
+        print("click")
+        LC_LOAD_DYLIB
+        
+        let careCardViewController = OCKCareCardViewController(carePlanStore: storeManager.store)
+        print("click")
+        self.navigationController?.pushViewController(careCardViewController, animated: true)
+        print("click")
+        
+       // self.navigationController?.pushViewController(careCardViewController, animated: true)
+    }
+
+    
+}
+
+
+
+extension ViewController: CarePlanStoreManagerDelegate {
+    
+    /// Called when the `CarePlanStoreManager`'s insights are updated.
+    func carePlanStoreManager(manager: CarePlanStoreManager, didUpdateInsights insights: [OCKInsightItem]) {
+        // Update the insights view controller with the new insights.
+        //insightsViewController.items = insights
+        
+    }
+}
